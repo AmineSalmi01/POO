c7134b3540f758d7680edfe4af28339c4b3f50f6
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2023-01-24 21:46:28 +0100

    .

---
+++ Constructor.php
@@ -0,0 +1,22 @@
+<?php
+class Person {
+    public $name;
+    public $age;
+
+    function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+    function get_age() {
+        return $this->age;
+    }
+}
+$amine = new Person('amine', '22');
+echo $amine->get_name();
+echo 'br';
+echo $amine->get_age();
+?>
