bfd46eac398f7f29ff9237622e29eee499d1bd5a
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2023-01-29 18:35:43 +0100

    .

--- POO_VALIDATION/Poo_exercis.php
+++ POO_VALIDATION/Poo_exercis.php
@@ -28,21 +28,24 @@ class Country extends Capital {
     }
 }
 
-interface CapitalInterface{
+interface CapitalInterface {
+
     public function insert($item);
     public function delete($item);
     public static function stock($item);
+
 }
 
 class managmentCity implements CapitalInterface{
-    public $count = 0;
+
+    public static $count = 0;
     public static $data;
 
     public function insert($item)
     {
-        $this->count++;
+        Self::$count++;
         Self::stock($item);
-        return ['counter' => $this->count , 'data' => Self::$data];
+        return ['counter' => Self::$count , 'data' => Self::$data];
     }
 
     public static function stock($item)
@@ -54,25 +57,69 @@ class managmentCity implements CapitalInterface{
     {
         $index_key = array_search($item->displaye(), Self::$data);
         unset(Self::$data[$index_key]);
-        $this->count--;
-        return ['counter' => $this->count, 'data'=> Self::$data];
+        Self::$count--;
+        return ['counter' => Self::$count, 'data'=> Self::$data];
     }
 }
 
+// country 
+class managmentCounyrt implements CapitalInterface {
+
+    public static $count = 0;
+    public static $data;
+
+    public function insert($item)
+    {
+        Self::$count++;
+        Self::stock($item);
+        return ['counter' => Self::$count , 'data' => Self::$data];
+    }
+
+    public static function stock($item)
+    {
+        Self::$data[] = $item;
+    }
+
+    public function delete($item)
+    {
+        $index_key = array_search($item->displaye(), Self::$data);
+        unset(Self::$data[$index_key]);
+        Self::$count--;
+        return ['counter' => Self::$count, 'data'=> Self::$data];
+    }
+} 
+
 echo '<pre>';
+echo 'City <br>';
 $city = new City('tanger', 'arabic');
 $city1 = new City('roma', 'talian');
-print_r($city->displaye());
+$city2 = new City('titwan', 'arabic');
+// print_r($city->displaye());
 $managmentCity = new managmentCity();
+$managmentCity2 = new managmentCity();
 
 print_r($managmentCity->insert($city1->displaye()));
 print_r($managmentCity->insert($city->displaye()));
 print_r($managmentCity->delete($city));
 
+print_r($managmentCity2->insert($city2->displaye()));
+
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
 echo '<br>';
 
+
+echo 'country <br>';
 $Country = new Country('maroc', 'arabic');
-print_r($Country->displaye());
+$Country1 = new Country('France', 'francais');
+$managmentCounyrt = new managmentCounyrt();
+print_r($managmentCounyrt->insert($Country->displaye()));
+print_r($managmentCounyrt->insert($Country1->displaye()));
+print_r($managmentCounyrt->delete($Country1));
+
+// print_r($Country->displaye());
 echo '</pre>';  
 ?>
 
